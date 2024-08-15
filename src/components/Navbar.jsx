bf93bd24532383b418dcb2cab4e17092efa5eb4f
ogooluwa <ogooluwaakinleye29@gmail.com>
2024-08-15 05:47:19 -0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,90 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-
 const Navbar = () => {
+  const [isCasesDropdownOpen, setIsCasesDropdownOpen] = useState(false);
+  const [isFilesDropdownOpen, setIsFilesDropdownOpen] = useState(false);
+  const [isTransactionsDropdownOpen, setIsTransactionsDropdownOpen] = useState(false);
+  const [isPropertiesDropdownOpen, setIsPropertiesDropdownOpen] = useState(false);
+
+  const toggleCasesDropdown = () => {
+    setIsCasesDropdownOpen(!isCasesDropdownOpen);
+    setIsFilesDropdownOpen(false); // Close Files dropdown
+    setIsTransactionsDropdownOpen(false); // Close Transactions dropdown
+    setIsPropertiesDropdownOpen(false); // Close Properties dropdown
+  };
+
+  const toggleFilesDropdown = () => {
+    setIsFilesDropdownOpen(!isFilesDropdownOpen);
+  };
+
+  const toggleTransactionsDropdown = () => {
+    setIsTransactionsDropdownOpen(!isTransactionsDropdownOpen);
+  };
+
+  const togglePropertiesDropdown = () => {
+    setIsPropertiesDropdownOpen(!isPropertiesDropdownOpen);
+  };
+
   return (
-     <div>
+    <div>
       <div className="sidebar">
         <div className="icon">
           <a href="#">
-      <i class="fa-solid fa-scale-balanced"></i>
-        <div className="logo">LEGAL PAD</div></a>
+            <i className="fa-solid fa-scale-balanced"></i>
+            <div className="logo">LEGAL PAD</div>
+          </a>
         </div>
-        <ul  className="menu">
-          <li className='active'>
-            <a href="#">
-          <i class="fa-solid fa-user"></i><Link to='/' className='nav-item'>Client</Link></a>
-          </li>
+        <ul className="menu">
           <li>
-            <a href="#">
-            <i class="fa-solid fa-person"></i><Link to='/staff' className='nav-item'>Staff</Link></a>
-          </li>
-          <li>
-            <a href="#">
-            <i class="fa-solid fa-folder"></i><Link to='/cases' className='nav-item'>Cases</Link></a>
-          </li>
-          <li>
-            <a href="#">
-            <i class="fa-solid fa-check"></i><Link to='/taskapprv' className='nav-item'>Task Approval</Link></a>
-          </li>
-          <li>
-            <a href="#">
-            <i class="fa-solid fa-list"></i><Link to='/todo' className='nav-item'>Todo List</Link></a>
-          </li>
-          <li>
-            <a href="#">
-            <i class="fa-solid fa-sitemap"></i><Link to='/tasktrans' className='nav-item'>Todo Items</Link></a>
-          </li>
-          <li>
-            <a href="#">
-            <i class="fa-solid fa-clipboard-check"></i><Link to='/taskmove' className='nav-item'>Todo Results</Link></a>
+            <a href="#" onClick={toggleCasesDropdown}>
+              <i className="fa-solid fa-folder"></i>
+              Cases
+            </a>
+            {isCasesDropdownOpen && (
+              <ul className="menu">
+                <li>
+                  <a href="#" onClick={toggleFilesDropdown}>
+                    Files
+                  </a>
+                  {isFilesDropdownOpen && (
+                    <ul className="menu">
+                      <li><Link to="/" className="nav-item">Client</Link></li>
+                      <li><Link to="/staff" className="nav-item">Staff</Link></li>
+                      <li><Link to="/taskapprv" className="nav-item">Task Approval</Link></li>
+                      <li><Link to="/todo" className="nav-item">Todo List</Link></li>
+                    </ul>
+                  )}
+                </li>
+                <li>
+                  <a href="#" onClick={toggleTransactionsDropdown}>
+                    Transactions
+                  </a>
+                  {isTransactionsDropdownOpen && (
+                    <ul className="menu">
+                      <li><Link to="/tasktrans" className="nav-item">Todo Items</Link></li>
+                      <li><Link to="/taskmove" className="nav-item">Todo Results</Link></li>
+                    </ul>
+                  )}
+                </li>
+                <li>
+                  <a href="#" onClick={togglePropertiesDropdown}>
+                    Reports
+                  </a>
+                  {isPropertiesDropdownOpen && (
+                    <ul className="menu">
+                      <li><Link to="/property1" className="nav-item">Property 1</Link></li>
+                      <li><Link to="/property2" className="nav-item">Property 2</Link></li>
+                    </ul>
+                  )}
+                </li>
+              </ul>
+            )}
           </li>
         </ul>
       </div>
-  </div>
-  )
+    </div>
+  );
 };
 
 export default Navbar;
